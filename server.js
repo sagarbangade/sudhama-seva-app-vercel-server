efa2ab64bf6db7133a80a3776d7a3ab08df100e4
sagar.b <sagar.b@hhstechgroup.com>
2025-06-18 02:03:28 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 
